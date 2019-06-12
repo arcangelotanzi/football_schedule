dd539fac7a179129ba9598cfede52c065e639425
Arcangelo <arcangelotanzi@gmai.com>
2019-06-12 19:01:02 +0200

    new components

--- footballSchedule/src/app/app.module.ts
+++ footballSchedule/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MatchComponent } from './match/match.component';
 import { HttpClientModule } from '@angular/common/http';
+import { TeamComponent } from './team/team.component';
+import { RankingComponent } from './ranking/ranking.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MatchComponent
+    MatchComponent,
+    TeamComponent,
+    RankingComponent
   ],
   imports: [
     HttpClientModule,
